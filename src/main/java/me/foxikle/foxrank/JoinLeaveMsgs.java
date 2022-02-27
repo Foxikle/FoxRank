6b5c21ea4df81cd3be8d5f044e6a0c945fe7d1cd
Foxikle <tristantroop260@gmail.com>
2022-02-27 11:18:34 -0500

    Added support for removing join and laeve messages. Fixed naming issues

---
+++ src/main/java/me/foxikle/foxrank/JoinLeaveMsgs.java
@@ -0,0 +1,27 @@
+package me.foxikle.foxrank;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinLeaveMsgs implements Listener {
+
+    @EventHandler (priority = EventPriority.MONITOR)
+    public void onPlayerLogin(PlayerJoinEvent event) {
+        if(FoxRank.getInstance().getConfig().getBoolean("DisableJoinMessages")) {
+            event.setJoinMessage(null);
+        }
+        event.setJoinMessage(ChatColor.YELLOW + event.getPlayer().getName() + "join the game.");
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (FoxRank.getInstance().getConfig().getBoolean("DisableLeaveMessages")) {
+            event.setQuitMessage(null);
+        }
+        event.setQuitMessage(ChatColor.YELLOW + event.getPlayer().getName() + "left the game.");
+    }
+}
