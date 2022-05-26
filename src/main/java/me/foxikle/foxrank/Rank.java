40496c5aef35616e308c34beaffd793baa455d43
Foxikle <tristantroop260@gmail.com>
2022-05-25 21:37:47 -0400

    Added the ability to get ranks from strings

--- src/main/java/me/foxikle/foxrank/Rank.java
+++ src/main/java/me/foxikle/foxrank/Rank.java
@@ -3,23 +3,23 @@ package me.foxikle.foxrank;
 import org.bukkit.ChatColor;
 
 public enum Rank {
-
-    NONE(ChatColor.GRAY + "", 1, 0),
-    OWNER(ChatColor.DARK_RED + "[OWNER] ", 100, 100),
-    ADMIN(ChatColor.RED + "[ADMIN] ", 90, 90),
-    MODERATOR(ChatColor.DARK_GREEN + "[MOD] ", 80, 80),
-    YOUTUBE(ChatColor.RED + "[" + ChatColor.WHITE + "YOUTUBE" + ChatColor.RED + "]", 70, 70),
-    TWITCH(ChatColor.DARK_PURPLE + "[" + ChatColor.WHITE + "TWITCH" + ChatColor.DARK_PURPLE + "]", 70, 70),
-    MVPp(ChatColor.AQUA + "[MVP" + ChatColor.WHITE + "+" + ChatColor.AQUA + "] ", 65, 65),
-    MVP(ChatColor.AQUA + "[MVP]", 60, 60),
-    VIPp(ChatColor.GREEN + "[VIP" + ChatColor.GOLD + "+" + ChatColor.GREEN + "] ", 45, 45),
-    VIP(ChatColor.GREEN + "[VIP] ", 40, 40);
+    DUMMY(ChatColor.LIGHT_PURPLE + "", 0, "DUMMY"),
+    DEFAULT(ChatColor.GRAY + "", 1, "DEFAULT"),
+    OWNER(ChatColor.RED + "[OWNER] ", 100, "OWNER"),
+    ADMIN(ChatColor.RED + "[ADMIN] ", 90, "ADMIN"),
+    MODERATOR(ChatColor.DARK_GREEN + "[MOD] ", 80, "MODERATOR"),
+    YOUTUBE(ChatColor.RED + "[" + ChatColor.WHITE + "YOUTUBE" + ChatColor.RED + "] ", 70, "YOUTUBE"),
+    TWITCH(ChatColor.DARK_PURPLE + "[" + ChatColor.WHITE + "TWITCH" + ChatColor.DARK_PURPLE + "] ", 70, "TWITCH"),
+    MVP_PLUS(ChatColor.AQUA + "[MVP" + ChatColor.WHITE + "+" + ChatColor.AQUA + "] ", 65, "MVP_PLUS"),
+    MVP(ChatColor.AQUA + "[MVP] ", 60, "MVP"),
+    VIP_PLUS(ChatColor.GREEN + "[VIP" + ChatColor.GOLD + "+" + ChatColor.GREEN + "] ", 45, "VIP_PLUS"),
+    VIP(ChatColor.GREEN + "[VIP] ", 40, "VIP");
 
     private final String prefix;
     private final int powerLevel;
-    private final int rankID;
+    private final String rankID;
 
-    Rank(String prefix, int powerLevel, int rankID) {
+    Rank(String prefix, int powerLevel, String rankID) {
         this.prefix = prefix;
         this.powerLevel = powerLevel;
         this.rankID = rankID;
@@ -29,11 +29,36 @@ public enum Rank {
         return prefix;
     }
 
-    public int getRankID() {
+    public String getRankID() {
         return rankID;
     }
 
     public int getPowerLevel(){
         return powerLevel;
+    }
+    public Rank getRankFromString(String s){
+        if (s.equalsIgnoreCase("DEFAULT")){
+            return Rank.DEFAULT;
+        } else if (s.equalsIgnoreCase("OWNER")){
+            return Rank.OWNER;
+        } else if (s.equalsIgnoreCase("ADMIN")){
+            return Rank.ADMIN;
+        } else if (s.equalsIgnoreCase("MODERATOR")){
+            return Rank.MODERATOR;
+        } else if (s.equalsIgnoreCase("YOUTUBE")){
+            return Rank.YOUTUBE;
+        } else if (s.equalsIgnoreCase("TWITCH")){
+            return Rank.TWITCH;
+        } else if (s.equalsIgnoreCase("MVP_PLUS")){
+            return Rank.MVP_PLUS;
+        } else if (s.equalsIgnoreCase("MVP")){
+            return Rank.MVP;
+        } else if (s.equalsIgnoreCase("VIP_PLUS")){
+            return Rank.VIP_PLUS;
+        } else if (s.equalsIgnoreCase("VIP")){
+            return Rank.VIP;
+        } else {
+            return Rank.DEFAULT;
+        }
     }
 }
