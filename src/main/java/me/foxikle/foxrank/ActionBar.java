710c0186558dc984d0825a5f071568e1fc259c3f
Foxikle <tristantroop260@gmail.com>
2023-02-11 07:18:12 -0500

    Added an option to disable the actionbar
    Signed-off-by: Foxikle <tristantroop260@gmail.com>

--- src/main/java/me/foxikle/foxrank/ActionBar.java
+++ src/main/java/me/foxikle/foxrank/ActionBar.java
@@ -9,18 +9,20 @@ import static net.md_5.bungee.api.ChatMessageType.ACTION_BAR;
 
 public class ActionBar {
     protected static void setupActionBar(Player player) {
-        new BukkitRunnable() {
-            public void run() {
-                if (FoxRank.getInstance().isNicked(player.getUniqueId()) && FoxRank.getInstance().isVanished(player.getUniqueId())) {
-                    player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "NICKED" + ChatColor.WHITE + " & " + ChatColor.RED + "VANISHED" + ChatColor.WHITE + "."));
-                } else if (FoxRank.getInstance().isVanished(player.getUniqueId())) {
-                    player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "VANISHED" + ChatColor.WHITE + "."));
-                } else if (FoxRank.getInstance().isNicked(player.getUniqueId())) {
-                    player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "NICKED" + ChatColor.WHITE + "."));
-                } else {
-                    this.cancel();
+        if (!FoxRank.getInstance().getConfig().getBoolean("DisableActionbar")) {
+            new BukkitRunnable() {
+                public void run() {
+                    if (FoxRank.getInstance().isNicked(player.getUniqueId()) && FoxRank.getInstance().isVanished(player.getUniqueId())) {
+                        player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "NICKED" + ChatColor.WHITE + " & " + ChatColor.RED + "VANISHED" + ChatColor.WHITE + "."));
+                    } else if (FoxRank.getInstance().isVanished(player.getUniqueId())) {
+                        player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "VANISHED" + ChatColor.WHITE + "."));
+                    } else if (FoxRank.getInstance().isNicked(player.getUniqueId())) {
+                        player.spigot().sendMessage(ACTION_BAR, TextComponent.fromLegacyText(ChatColor.WHITE + "You are currently " + ChatColor.RED + "NICKED" + ChatColor.WHITE + "."));
+                    } else {
+                        this.cancel();
+                    }
                 }
-            }
-        }.runTaskTimer(FoxRank.getInstance(), 0, 20);
+            }.runTaskTimer(FoxRank.getInstance(), 0, 20);
+        }
     }
 }
