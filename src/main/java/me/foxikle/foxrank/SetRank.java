7e5ef6c31bb484a704af05e0288584b39746bd54
Foxikle <tristantroop260@gmail.com>
2023-03-25 11:55:08 -0400

    Added support for setrank across a proxy
    Signed-off-by: Foxikle <tristantroop260@gmail.com>

--- src/main/java/me/foxikle/foxrank/SetRank.java
+++ src/main/java/me/foxikle/foxrank/SetRank.java
@@ -38,17 +38,22 @@ public class SetRank implements CommandExecutor, TabExecutor {
                                 FoxRank.getInstance().sendInvalidArgsMessage("Rank", new RankedPlayer(player));
                             }
                         } else if (args.length >= 2) {
-                                Player p = Bukkit.getServer().getPlayer(args[1]);
-                                if (getRank(player).getPowerLevel() > getRank(p).getPowerLevel()) {
-                                    if (rankList.contains(args[0])) {
-                                        Rank rank = Rank.valueOf(args[0]);
-                                        foxRank.setRank(p, rank);
-                                        new RankedPlayer(p).sendMessage(ChatColor.translateAlternateColorCodes('§', FoxRank.getInstance().getConfig().getString("RankIsNowMessage").replace("$RANK", Rank.valueOf(args[0]).getPrefix())));
-                                        foxRank.loadRank(player);
-                                        foxRank.loadRank(p);
-                                    } else {
-                                        FoxRank.getInstance().sendInvalidArgsMessage("Rank", new RankedPlayer(player));
-                                    }
+                            if (foxRank.bungeecord) {
+                                FoxRank.getInstance().setRankOfflinePlayer(Bukkit.getOfflinePlayer(args[1]), Rank.ofString(args[0]));
+                                FoxRank.getPluginChannelListener().sendUpdateDataMessage(args[1]);
+                                FoxRank.getPluginChannelListener().sendMessage(player, args[1], ChatColor.translateAlternateColorCodes('§', FoxRank.getInstance().getConfig().getString("RankIsNowMessage").replace("$RANK", Rank.valueOf(args[0]).getPrefix())));
+                                return true;
+                            }
+                            Player p = Bukkit.getServer().getPlayer(args[1]);
+                            if (getRank(player).getPowerLevel() > getRank(p).getPowerLevel()) {
+                                if (rankList.contains(args[0])) {
+                                    Rank rank = Rank.valueOf(args[0]);
+                                    foxRank.setRank(p, rank);
+                                    new RankedPlayer(p).sendMessage(ChatColor.translateAlternateColorCodes('§', FoxRank.getInstance().getConfig().getString("RankIsNowMessage").replace("$RANK", Rank.valueOf(args[0]).getPrefix())));
+                                    foxRank.loadRank(p);
+                                } else {
+                                    FoxRank.getInstance().sendInvalidArgsMessage("Rank", new RankedPlayer(player));
+                                }
                                 } else {
                                     player.sendMessage(ChatColor.RED + "Your rank must be higher than " + new RankedPlayer(p).getPrefix() + new RankedPlayer(p).getName() + " to change their rank!");
                                 }
