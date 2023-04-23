596ee0c055ef86016fb8a6f27cc7e1753198c422
Foxikle <tristantroop260@gmail.com>
2023-04-23 17:36:24 -0400

    added method to get the old rank of a player on RankChangeEvent
    Signed-off-by: Foxikle <tristantroop260@gmail.com>

--- src/main/java/me/foxikle/foxrank/events/RankChangeEvent.java
+++ src/main/java/me/foxikle/foxrank/events/RankChangeEvent.java
@@ -8,10 +8,12 @@ import org.bukkit.event.HandlerList;
 public class RankChangeEvent extends Event {
     private final Player player;
     private final Rank newRank;
+    private final Rank oldRank;
 
-    public RankChangeEvent(Player player, Rank newRank) {
+    public RankChangeEvent(Player player, Rank newRank, Rank oldRank) {
         this.player = player;
         this.newRank = newRank;
+        this.oldRank = oldRank;
     }
 
     /**
@@ -26,15 +28,27 @@ public class RankChangeEvent extends Event {
     }
 
     /**
-     * <p> Gets the rank of the player
+     * <p> Gets the new rank of the player
      * </p>
      *
-     * @return the rank of player
+     * @return the new rank of player
      * @see Rank Please see the {@link me.foxikle.foxrank.Rank}
      * @since 1.8.1
      */
     public Rank getNewRank() {
         return newRank;
+    }
+
+    /**
+     * <p> Gets the old rank of the player
+     * </p>
+     *
+     * @return the old rank of player
+     * @see Rank Please see the {@link me.foxikle.foxrank.Rank}
+     * @since 1.9.3
+     */
+    public Rank getOldRank() {
+        return oldRank;
     }
 
     /**
