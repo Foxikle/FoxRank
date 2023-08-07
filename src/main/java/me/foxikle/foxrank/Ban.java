a49160087b18d03b051f11c803f8938fbaa4cf8a
Foxikle <tristantroop260@gmail.com>
2023-08-06 21:31:05 -0400

    DatA cAcHiNg!

--- src/main/java/me/foxikle/foxrank/Ban.java
+++ src/main/java/me/foxikle/foxrank/Ban.java
@@ -1,8 +1,7 @@
 package me.foxikle.foxrank;
 
-import com.google.gson.JsonParser;
+import me.foxikle.foxrank.Data.DataManager;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,24 +11,19 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 import static java.util.Objects.hash;
-import static me.foxikle.foxrank.ModerationAction.banOfflinePlayer;
-import static me.foxikle.foxrank.ModerationAction.banPlayer;
 
 
 public class Ban implements CommandExecutor, TabCompleter {
     private final FoxRank plugin;
     private final FileConfiguration yml;
-    private String reason = "No reason specified.";
-    private Instant expires;
     private boolean silent = false;
-    private String banID = null;
 
     public Ban(FoxRank plugin) {
         this.plugin = plugin;
@@ -37,15 +31,23 @@ public class Ban implements CommandExecutor, TabCompleter {
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (label.equalsIgnoreCase("ban")) {
+        if (label.equalsIgnoreCase("ban") || label.equalsIgnoreCase("foxrank:ban")) {
             if (sender instanceof Player banner) {
-                RankedPlayer staff = new RankedPlayer(banner, plugin);
                     if (args.length >= 4) {
+                        String banID;
+                        Instant expires;
+                        String reason;
                         if (Bukkit.getServer().getPlayer(args[0]) != null) {
                             Player banee = Bukkit.getServer().getPlayer(args[0]);
-                            RankedPlayer baneeRp = new RankedPlayer(banee, plugin);
-                            if (banee.hasPermission("foxrank.moderation.ban.immune") || baneeRp.getPowerLevel() >= staff.getPowerLevel()) {
-                                staff.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("CannotBanPlayerMessage")));
+                            plugin.targetMap.put(banner.getUniqueId(), banee.getUniqueId());
+                            Rank baneeRank = plugin.getPlayerData(banee.getUniqueId()).getRank();
+                            Rank staffRank = plugin.getPlayerData(banner.getUniqueId()).getRank();
+                            if (banee.hasPermission("foxrank.moderation.ban.immune")){
+                                banner.sendMessage(plugin.getMessage("CannotBanImmunePlayer", banner));
+                                return true;
+                            } else if (baneeRank.getPowerlevel() >= staffRank.getPowerlevel()) {
+                                banner.sendMessage(plugin.getMessage("CannotBanSuperiorPlayerMessage", banner));
+                                return true;
                             } else {
                                 ConfigurationSection section = yml.getConfigurationSection("BanReasons");
                                 if (args[1].equalsIgnoreCase("CUSTOM")) {
@@ -58,7 +60,8 @@ public class Ban implements CommandExecutor, TabCompleter {
                                 } else if (section.contains(args[1])) {
                                     reason = section.getString(args[1]);
                                 } else {
-                                    staff.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPresetBanReason")));
+                                    plugin.attemptedBanPresetMap.put(banner.getUniqueId(), args[1]);
+                                    banner.sendMessage(plugin.getMessage("NoPresetBanReason", banner));
                                     return true;
                                 }
                                 if (args[2].contains("d") || args[2].contains("h") || args[2].contains("m") || args[2].equalsIgnoreCase("-1")) {
@@ -66,14 +69,14 @@ public class Ban implements CommandExecutor, TabCompleter {
                                     String durStr = args[2];
                                     if (args[2].equalsIgnoreCase("-1")) {
                                         if (!banner.hasPermission("foxrank.moderation.ban.permanent")) {
-                                            banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                            banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                             return true;
                                         }
 
                                         expires = null;
                                     } else if (args[2].contains("d")) {
                                         if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                            banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                            banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                             return true;
                                         }
                                         durStr = durStr.replace("d", "");
@@ -81,7 +84,7 @@ public class Ban implements CommandExecutor, TabCompleter {
                                         expires = Instant.now().plusSeconds((long) durInt * 24 * 60 * 60);
                                     } else if (args[2].contains("h")) {
                                         if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                            banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                            banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                             return true;
                                         }
                                         durStr = durStr.replace("h", "");
@@ -89,7 +92,7 @@ public class Ban implements CommandExecutor, TabCompleter {
                                         expires = Instant.now().plusSeconds((long) durInt * 60 * 60);
                                     } else if (args[2].contains("m")) {
                                         if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                            banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                            banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                             return true;
                                         }
                                         durStr = durStr.replace("m", "");
@@ -112,22 +115,27 @@ public class Ban implements CommandExecutor, TabCompleter {
                                 }
                                 reason = removeUnderScore(reason);
                                 banID = getBanID(banee);
-                                banPlayer(staff, banee, silent, reason, expires, reason, banID);
+                                ModerationAction.banPlayer(banner, banee, silent, reason, expires, reason, banID);
 
                             }
                         } else {
                             if (plugin.players.contains(args[0])) {
-                                OfflinePlayer banee = Bukkit.getServer().getOfflinePlayer(getUUID(args[0]));
+                                OfflinePlayer banee = Bukkit.getServer().getOfflinePlayer(DataManager.getUUID(args[0]));
                                 if (banee != null) {
-                                    OfflineRankedPlayer baneeRp = new OfflineRankedPlayer(banee);
-                                    if (baneeRp.getPowerLevel() >= staff.getPowerLevel()) {
-                                        staff.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("CannotBanPlayerMessage")));
+                                    Rank baneeRank = plugin.getPlayerData(banee.getUniqueId()).getRank();
+                                    Rank staffRank = plugin.getPlayerData(banner.getUniqueId()).getRank();
+                                    if (baneeRank.getPermissionNodes().contains("foxrank.moderation.ban.immune")){
+                                        banner.sendMessage(plugin.getMessage("CannotBanImmunePlayer", banner));
+                                        return true;
+                                    } else if (baneeRank.getPowerlevel() >= staffRank.getPowerlevel()) {
+                                        banner.sendMessage(plugin.getMessage("CannotBanSuperiorPlayerMessage", banner));
+                                        return true;
                                     } else {
                                         ConfigurationSection section = yml.getConfigurationSection("BanReasons");
                                         if (section.contains(args[1])) {
                                             reason = section.getString(args[1]);
                                         } else {
-                                            staff.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPresetBanReason")));
+                                            banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                             return true;
                                         }
                                         if (args[2].contains("d") || args[2].contains("h") || args[2].contains("m") || args[2].equalsIgnoreCase("-1")) {
@@ -135,13 +143,13 @@ public class Ban implements CommandExecutor, TabCompleter {
                                             String durStr = args[2];
                                             if (args[1].equalsIgnoreCase("-1")) {
                                                 if (!banner.hasPermission("foxrank.moderation.ban.permanent")) {
-                                                    banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                                    banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                                     return true;
                                                 }
                                                 expires = null;
                                             } else if (args[2].contains("d")) {
                                                 if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                                    banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                                    banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                                     return true;
                                                 }
                                                 durStr = durStr.replace("d", "");
@@ -149,7 +157,7 @@ public class Ban implements CommandExecutor, TabCompleter {
                                                 expires = Instant.now().plusSeconds((long) durInt * 24 * 60 * 60);
                                             } else if (args[2].contains("h")) {
                                                 if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                                    banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                                    banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                                     return true;
                                                 }
                                                 durStr = durStr.replace("h", "");
@@ -157,7 +165,7 @@ public class Ban implements CommandExecutor, TabCompleter {
                                                 expires = Instant.now().plusSeconds((long) durInt * 60 * 60);
                                             } else if (args[2].contains("m")) {
                                                 if (!banner.hasPermission("foxrank.moderation.ban.temporary")) {
-                                                    banner.sendMessage(ChatColor.translateAlternateColorCodes('§', yml.getString("NoPermissionMessage")));
+                                                    banner.sendMessage(plugin.getMessage("NoPermissionMessage", banner));
                                                     return true;
                                                 }
                                                 durStr = durStr.replace("m", "");
@@ -180,7 +188,7 @@ public class Ban implements CommandExecutor, TabCompleter {
                                         }
                                         reason = removeUnderScore(reason);
                                         banID = getBanID(banee);
-                                        banOfflinePlayer(staff, banee, reason, expires, reason, banID);
+                                        ModerationAction.banOfflinePlayer(banner, banee, reason, expires, reason, banID);
 
                                     }
                                 }
@@ -229,18 +237,5 @@ public class Ban implements CommandExecutor, TabCompleter {
 
     private String removeUnderScore(String string) {
         return string.replace("_", "");
-    }
-
-    private UUID getUUID(String name) {
-        URL url;
-        InputStreamReader reader = null;
-        try {
-            url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
-            reader = new InputStreamReader(url.openStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String raw = new JsonParser().parse(reader).getAsJsonObject().get("id").getAsString();
-        return UUID.fromString(raw.substring(0, 8) + "-" + raw.substring(8, 12) + "-" + raw.substring(12, 16) + "-" + raw.substring(16, 20) + "-" + raw.substring(20, 32));
     }
 }
