d39912de0cfe1f04b792d1409dd17aade75e4b32
Foxikle <tristantroop260@gmail.com>
2023-08-06 21:26:44 -0400

    New message system, chached data

--- src/main/java/me/foxikle/foxrank/Unban.java
+++ src/main/java/me/foxikle/foxrank/Unban.java
@@ -8,18 +8,27 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class Unban implements CommandExecutor, TabCompleter {
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+    private final FoxRank plugin;
+
+    public Unban(FoxRank plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, String label, String[] args) {
         if (label.equalsIgnoreCase("unban")) {
             if (sender instanceof Player player) {
                 if (player.hasPermission("foxrank.moderation.unban")) {
                     if (args.length >= 1) {
                         OfflinePlayer op = Bukkit.getOfflinePlayer(args[0]);
+                        plugin.targetMap.put(player.getUniqueId(), op.getUniqueId());
                         if (FoxRank.getInstance().getDm().getBannedPlayers().contains(op.getUniqueId())) {
                             ModerationAction.unbanPlayer(op.getUniqueId(), player.getUniqueId());
                             player.sendMessage(ChatColor.GREEN + op.getName() + " was unbanned.");
@@ -27,7 +36,8 @@ public class Unban implements CommandExecutor, TabCompleter {
                             player.sendMessage(ChatColor.RED + op.getName() + " is not banned!");
                         }
                     } else {
-                        FoxRank.getInstance().sendInvalidArgsMessage("Player", player);
+                        FoxRank.getInstance().syntaxMap.put(player.getUniqueId(), "/unban <player>");
+                        player.sendMessage(FoxRank.getInstance().getSyntaxMessage(player));
                     }
                 } else {
                     player.sendMessage(FoxRank.getInstance().getMessage("NoPermissionMessage", player));
