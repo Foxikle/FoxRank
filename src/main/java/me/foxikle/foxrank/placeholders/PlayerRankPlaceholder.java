90f773ec072668a6bc2f7e0446df5bd7b43b68bf
Foxikle <tristantroop260@gmail.com>
2023-08-13 21:13:33 -0400

    Added offline player support

--- src/main/java/me/foxikle/foxrank/placeholders/PlayerRankPlaceholder.java
+++ src/main/java/me/foxikle/foxrank/placeholders/PlayerRankPlaceholder.java
@@ -2,9 +2,12 @@ package me.foxikle.foxrank.placeholders;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.foxikle.foxrank.FoxRank;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
 
 public class PlayerRankPlaceholder extends PlaceholderExpansion {
     private final FoxRank plugin;
@@ -40,14 +43,30 @@ public class PlayerRankPlaceholder extends PlaceholderExpansion {
 
     @Override
     public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
+        UUID uuid = player.getUniqueId();
         if (params.equalsIgnoreCase("prefix")) {
             return plugin.getRank(player).getPrefix();
         } else if (params.equalsIgnoreCase("color")) {
-            return String.valueOf(plugin.getRank(player).getColor());
+            return String.valueOf(plugin.getPlayerData(uuid).getRank().getColor());
         } else if (params.equalsIgnoreCase("id")) {
             return plugin.getRank(player).getId();
         } else if (params.equalsIgnoreCase("textcolor")) {
             return String.valueOf(plugin.getRank(player).getTextColor());
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
+        UUID uuid = player.getUniqueId();
+        if (params.equalsIgnoreCase("prefix")) {
+            return plugin.getPlayerData(uuid).getRank().getPrefix();
+        } else if (params.equalsIgnoreCase("color")) {
+            return String.valueOf(plugin.getPlayerData(uuid).getRank().getColor());
+        } else if (params.equalsIgnoreCase("id")) {
+            return plugin.getPlayerData(uuid).getRank().getId();
+        } else if (params.equalsIgnoreCase("textcolor")) {
+            return String.valueOf(plugin.getPlayerData(uuid).getRank().getTextColor());
         }
         return null;
     }
